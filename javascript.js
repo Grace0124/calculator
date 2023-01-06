b7a96638c01b584755819143ea931832df8d434b
Grace0124 <lyuhan@gmail.com>
2023-01-05 21:07:39 -0500

    Added display buttons and screen. Added JS onclick events.

--- javascript.js
+++ javascript.js
@@ -18,6 +18,61 @@ function operate(operator, x, y) {
     return operator(x, y);
 }
 
-console.log(
-    operate(add, 4, 5)
-)
+function updateNumbers(char, storedNum, currentNum, displayDiv) {
+    if (storedNum == Infinity) {
+        storedNum = char;
+        displayDiv.textContent = storedNum;
+    } else {
+        currentNum = char;
+    }
+    console.log({storedNum});
+    return (storedNum, currentNum);
+} 
+
+
+function updateOperator(char) {
+    switch(char) {
+        case '+':
+            return add;
+        case '-':
+            return subtract;
+        case '×':
+            return multiply;
+        case '÷':
+            return divide;
+    }        
+}
+
+
+
+function display(char, storedNum, currentNum, operator, result) {
+    
+
+    
+
+}
+
+const displayDiv = document.querySelector("#display");
+let storedNum = Infinity;
+let currentNum = Infinity;
+let operator;
+let result;
+
+if (char === 'clear') {
+    storedNum = Infinity;
+    currentNum = Infinity;
+    operator = undefined;
+} else if (char === '=') {
+    console.log({operator});
+    result = operate(operator, storedNum, currentNum);
+    displayDiv.textContent = result;
+    storedNum = result;
+} else if (typeof(char) == "string") {
+    operator = updateOperator(char);
+    console.log({operator});
+} else {
+    storedNum, currentNum = updateNumbers(char, storedNum, currentNum, displayDiv);
+}
+
+
+document.getElementById("1").onclick = function fun()
